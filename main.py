806942c7440b21e1e11ac7808d7a9c99d5142ffc
Rayhanaf <120548531+Rayhanaf@users.noreply.github.com>
2023-01-26 10:43:22 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def on_forever():
-    serial.write_value("earthquake", pins.digital_read_pin(DigitalPin.P1))
-    serial.write_value("level", pins.digital_read_pin(DigitalPin.P2))
-    if pins.digital_read_pin(DigitalPin.P1) == 1 and pins.digital_read_pin(DigitalPin.P2) >= 200:
+    serial.write_value("earthquake", pins.analog_read_pin(AnalogPin.P1))
+    basic.show_number(pins.analog_read_pin(AnalogPin.P1))
+basic.forever(on_forever)
+
+def on_forever2():
+    if pins.digital_read_pin(DigitalPin.P1) == 1:
         pins.digital_write_pin(DigitalPin.P0, 1)
         basic.show_number(1)
-        music.start_melody(music.built_in_melody(Melodies.DADADADUM),
-            MelodyOptions.ONCE)
+        music.start_melody(music.built_in_melody(Melodies.POWER_DOWN),
+            MelodyOptions.FOREVER)
         basic.pause(2000)
     pins.digital_write_pin(DigitalPin.P0, 0)
     basic.show_number(0)
-basic.forever(on_forever)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showNumber(1)
-        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Forever)
+        music.playMelody("G F E B D C C5 G ", 500)
         basic.pause(2000)
     }
     pins.digitalWritePin(DigitalPin.P0, 0)
