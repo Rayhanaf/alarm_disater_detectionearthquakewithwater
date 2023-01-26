3582a9f01393dff1167eabda13da1f14caf44aad
Rayhanaf <120548531+Rayhanaf@users.noreply.github.com>
2023-01-26 11:07:19 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 basic.forever(function () {
     serial.writeValue("earthquake", pins.analogReadPin(AnalogPin.P1))
+    serial.writeValue("level", pins.analogReadPin(AnalogPin.P2))
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+    if (pins.analogReadPin(AnalogPin.P1) >= 1004 && pins.analogReadPin(AnalogPin.P2) <= 300) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showNumber(1)
         music.playMelody("G F E B D C C5 G ", 500)
